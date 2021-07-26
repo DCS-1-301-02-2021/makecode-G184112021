bfa2ce01a7b44610d2b818f58026cdee022a722f
G184112021 <86590418+G184112021@users.noreply.github.com>
2021-07-26 16:46:03 +0900

    ゲームを完成させた

--- main.ts
+++ main.ts
@@ -1,1 +1,56 @@
 scene.setBackgroundColor(8)
+let spacePlane = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . 1 . . . . . . . . . . .
+    . . . . b b b f f f . . . . . .
+    . . . c b b b b . . . . . . . .
+    . . . c c b b b b . . . . . . .
+    . 2 2 c b c b b b b . . 1 1 1 .
+    2 4 4 c b b c b b b b b b b b f
+    . 2 2 b b b b b b b b b b b . .
+    . . . . b b b b b b . . . . . .
+    . . . . b b b b b . . . . . . .
+    . . . . b b b b . . . . . . . .
+    . . . . b b b . . . . . . . . .
+    . . . . b b f f f f . . . . . .
+`, SpriteKind.Player)
+info.setLife(3)
+spacePlane.setStayInScreen(true)
+controller.moveSprite(spacePlane, 200, 200)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    let missile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            4 4 4 4 4 4 4 4 . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, spacePlane, 200, 0)
+})
+game.onUpdateInterval(500, function on_update() {
+    let body = sprites.create(assets.image`body`, SpriteKind.Enemy)
+    body.setVelocity(-100, randint(-30, 30))
+    body.y = randint(0, scene.screenHeight())
+    body.left = scene.screenWidth()
+    body.setFlag(SpriteFlag.AutoDestroy, true)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_hit(sprite: Sprite, othersprite: Sprite) {
+    othersprite.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_crash(sprite: Sprite, othersprite: Sprite) {
+    othersprite.destroy()
+    info.changeLifeBy(-1)
+})
